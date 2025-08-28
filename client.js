ae853cda2269aa29d943624df3c7bf38050826f3
Bartłomiej Kozanka <b.kozanka@qarbon.it>
2025-08-28 22:32:20 +0200

    add webtorrent add/remove,

--- client.js
+++ client.js
@@ -1,20 +1,70 @@
-import WebSocket from 'ws';
+import WebSocket from "ws";
+import WebTorrent from "webtorrent";
+
 const address = process.env.ADDRESS;
 const ws = new WebSocket(address);
+const webtorrent = new WebTorrent();
 
-ws.on('open', () => {
+const TRACKERS = [
+  "udp://tracker.opentrackr.org:1337/announce",
+  "udp://tracker.openbittorrent.com:6969/announce",
+  "wss://tracker.openwebtorrent.com",
+  "wss://tracker.btorrent.xyz",
+  "wss://tracker.fastcast.nz",
+];
+
+const addTorrent = (torrentId) => {
+  try {
+    webtorrent.add(
+      // templatka magneta
+      `magnet:?xt=urn:btih:${torrentId}&tr=http%3A%2F%2Fnyaa.tracker.wf%3A7777%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce`,
+      { announce: TRACKERS },
+      (torrent) => {
+        resolve(torrent);
+      }
+    );
+    console.log("torrent added sucesfull");
+  } catch (err) {
+    console.log("torrent add error", err);
+  }
+};
+
+const removeTorrent = async (torrentId) => {
+  const torrent = webtorrent.get(torrentId);
+  if (torrent) {
+    webtorrent.remove(torrentId);
+    console.log(`Torrent ${torrentId} remove`);
+  } else {
+    console.log(`Torrent ${torrentId} not exist`);
+  }
+};
+
+ws.on("open", () => {
   console.log(`Połączono z ${address}`);
 });
 
-ws.on('message', (data) => {
-  console.log('Otrzymano wiadomość od serwera:', data.toString());
+ws.on("message", async (data) => {
+  console.log("Otrzymano wiadomość od serwera:", data.toString());
+  try {
+    const obj = JSON.parse(data);
+    console.log(obj.torrentId);
+    if (obj.torrentId && ["add", "remove"].includes(obj.action)) {
+      obj.action === "add"
+        ? addTorrent(obj.torrentId)
+        : removeTorrent(obj.torrentId);
+    } else {
+      ws.send("błędna wiadomość");
+    }
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-ws.on('close', () => {
-  console.log('serwer został rozłączony');
-  ws.terminate()
+ws.on("close", () => {
+  console.log("serwer został rozłączony");
+  ws.terminate();
 });
 
-ws.on('error', (err) => {
-  console.error('Błąd klienta:', err);
+ws.on("error", (err) => {
+  console.error("Błąd klienta:", err);
 });
--- server.js
+++ server.js
@@ -1,30 +1,24 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer } from "ws";
 
 const PORT = 8080;
 const wss = new WebSocketServer({ port: PORT });
 
-wss.on('connection', (ws) => {
-  console.log('Klient połączony z Serwerem!');
+wss.on("connection", (ws) => {
+  console.log("Klient połączony z Serwerem!");
 
-  setInterval(function ping() {
-  ws.clients.forEach(function each(ws) {
-    if (ws.isAlive === false) return ws.terminate();
-    ws.isAlive = false;
-    ws.ping();
-  });
-}, 30000);
+  ws.send(JSON.stringify({ torrentId: 1, action: 'add' }), console.log )
 
-  ws.on('message', (message) => {
-    console.log('Serwer otrzymał wiadomość:', message.toString());
+  ws.on("message", (message) => {
+    console.log("Serwer otrzymał wiadomość:", message.toString());
   });
 
-  ws.on('close', () => {
-    console.log('Klient rozłączony');
+  ws.on("close", () => {
+    console.log("Klient rozłączony");
     ws.terminate();
   });
 
-  ws.on('error', (err) => {
-    console.error('WebSocket error:', err);
+  ws.on("error", (err) => {
+    console.error("WebSocket error:", err);
   });
 });
 
